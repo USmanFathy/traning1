fb87835f9f46b397038bf45cbaa4201d02ca5b72
USmanFathy <osmanarrow90@gmail.com>
2023-02-07 09:33:32 -0800

    update new column

--- main.py
+++ main.py
@@ -23,16 +23,35 @@ company= soup.findAll('div' , class_='sc-ba46ca07-0 dROrNK job-card')
 
 comany_names = []
 comany_jobs = []
+job_links = []
+job_locations = []
+job_salaries = []
 
 for data in company:
     comany_names.append(data.find('div' , class_='company-name').text)
     comany_jobs.append(data.find('a' , class_='job-title').text)
+    job_links.append(f"https://arc.dev/remote-jobs{data.find('a' , class_='job-title')['href']}")
+
+for link in job_links:
+    page_two = requests.get(link)
+    page_two_content = page_two.content
+    soup_2 = BeautifulSoup(page_two_content , 'lxml')
+    div = soup_2.find('div' , class_= 'sc-a9bceba6-0 dtrGJF')
+
+    locations = div.find('div' , class_='sc-b9ad719a-0 jzcFMg').div.div.span.text
+    job_locations.append(locations)
+
+    salary = div.find('div' , class_='sc-b9ad719a-0 eSLzbJ').div.div.span.text
+    job_salaries.append(salary)
 
 
 
 data_jobs ={
     'company-name':comany_names , 
-    'job-titles' : comany_jobs
+    'job-titles' : comany_jobs,
+    'job-links' : job_links,
+    'job-location' : job_locations,
+    'job-salary' : job_salaries,
 }
 
 
